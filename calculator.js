f6fa67c73bbfcefe631de2c6f1d41781f476f5a6
arnorerling <arnorerling@gmail.com>
2018-10-13 11:04:48 +0000

    Forritið tekur inn eina tölu sem streng og skilar henni út aftur, 2 test passed

--- calculator.js
+++ calculator.js
@@ -1,6 +1,8 @@
 function add(a) {
    if(a == ""){
     return 0;
+   } else {
+    return parseInt(a); 
    }
   }
   module.exports = add; 
--- calculator.test.js
+++ calculator.test.js
@@ -3,4 +3,7 @@ const add = require('./calculator.js');
 test('returns 0', () => {
     expect(add("")).toBe(0);
   });
+test('returns the string or 1', () => {
+    expect(add("1")).toBe(1);
+  });
 
