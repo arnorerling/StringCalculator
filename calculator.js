8b90d108334f3815ae2422a994dbca3c1ec47109
arnorerling <arnorerling@gmail.com>
2018-10-13 11:16:54 +0000

    Forrit passar að notandi slái ekki inn mínus tölu, skilar lýsandi villuskiluboðum ef mínus tala er slegin inn, 7 test passed

--- calculator.js
+++ calculator.js
@@ -1,11 +1,19 @@
 function add(a) {
     var res = a.split(new RegExp ([',|\\n']));
     var sum = 0;
+    var negatives = [];
     if(a != ""){
         for(var i = 0; i < res.length; i++){
+            if(parseInt(res[i]) < 0){
+                negatives.push(res[i]);
+            }
             sum += parseInt(res[i]);
         }  
     } 
+    if(negatives.length > 0){
+        var errorMessage = 'Negatives not allowed:' + negatives;
+        throw new Error(errorMessage);
+    }
     return sum;
   }
   module.exports = add; 
--- calculator.test.js
+++ calculator.test.js
@@ -16,3 +16,18 @@ test('returns 1 + 2 + 3 + 4', () => {
     expect(add("1,2,3\n4")).toBe(10);
   });
 
+  test('returns negatives not allowed:-1,-2,-4', () => {
+    function testNeg() {
+      add("-1,-2,3\n-4");
+    }
+    expect(testNeg).toThrowError('Negatives not allowed:-1,-2,-4');
+    
+  });
+  test('returns negatives not allowed: -1', () => {
+    function testNeg() {
+      add("-1");
+    }
+    expect(testNeg).toThrowError('Negatives not allowed:-1');
+    
+  });
+
